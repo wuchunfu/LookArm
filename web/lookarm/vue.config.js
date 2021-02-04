87dc894bbf460112672c3ba8ec361ecbb5a3923e
wejectchan <weject@gmail.com>
2021-02-04 22:52:17 +0800

    fix bug

--- web/lookarm/vue.config.js
+++ web/lookarm/vue.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  "transpileDependencies": [
-    "vuetify"
-  ]
+  transpileDependencies: ['vuetify'],
+  chainWebpack: config => {
+    config.plugin('html').tap(args => {
+      args[0].title = 'LookArm - 快速搜索支持Mac M1的App'
+      return args
+    })
+  },
+  productionSourceMap: false
 }
