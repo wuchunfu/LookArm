2609133b5f2027ba7db66637d9208ad2795b689b
wejectchan <weject@gmail.com>
2021-02-22 11:50:17 +0800

    改进页面列表显示

--- web/lookarm/src/components/appList.vue
+++ web/lookarm/src/components/appList.vue
@@ -2,19 +2,11 @@
   <v-container>
     <v-row justify="center" align="center">
       <v-col cols="11" md="7">
-        <v-card
-          outlined
-          elevation="3"
-          class="my-4"
-          v-for="item in appInfoList"
-          :key="item.id"
-        >
+        <v-card outlined elevation="3" class="my-4" v-for="item in appInfoList" :key="item.id">
           <v-container>
             <v-row class="mt-3" align="center" justify="center" no-gutters>
               <v-col cols="8" md="8">
-                <v-card-title class="title text-h5 font-weight-bold headline">
-                  {{ item.app_name }}
-                </v-card-title>
+                <v-card-title class="title text-h5 font-weight-bold headline">{{ item.app_name }}</v-card-title>
               </v-col>
               <v-col cols="4" md="4">
                 <v-chip outlined label>{{ item.Category.name }}</v-chip>
@@ -25,40 +17,32 @@
           <v-card-text>
             <v-container>
               <v-row align="center" justify="center" no-gutters>
-                <v-col class="font-weight-bold" cols="12" md="4"
-                  >📋版本：{{ item.app_version }}</v-col
-                >
-                <v-col class="font-weight-bold" cols="12" md="4"
-                  >💡状态: {{ item.Tag.tag_name }}</v-col
-                >
-                <v-col class="font-weight-bold" cols="12" md="2"
-                  >🖥️开发: {{ item.app_developer }}</v-col
-                >
-                <v-col class="font-weight-bold" cols="12" md="2"
-                  >🕒更新: {{ item.UpdatedAt | dateFormat }}</v-col
-                >
+                <v-col class="font-weight-bold" cols="12" md="4">📋版本：{{ item.app_version }}</v-col>
+                <v-col class="font-weight-bold" cols="12" md="4">💡状态: {{ item.Tag.tag_name }}</v-col>
+                <v-col class="font-weight-bold" cols="12" md="2">🖥️开发: {{ item.app_developer }}</v-col>
+                <v-col
+                  class="font-weight-bold"
+                  cols="12"
+                  md="2"
+                >🕒更新: {{ item.UpdatedAt | dateFormat }}</v-col>
               </v-row>
             </v-container>
           </v-card-text>
 
-          <v-card-subtitle class="font-weight-bold"
-            >简短描述：{{ item.app_desc }}</v-card-subtitle
-          >
+          <v-card-subtitle class="mx-3 font-weight-bold">简短描述：{{ item.app_desc }}</v-card-subtitle>
 
           <v-divider class="mx-3"></v-divider>
           <v-card-actions>
             <v-row align="center" justify="start">
               <v-col cols="5" offset="1" md="5">
-                <v-btn :href="item.app_webpage" target="_blank" outlined
-                  >下载地址</v-btn
-                >
+                <v-btn :href="item.app_webpage" target="_blank" outlined>下载地址</v-btn>
               </v-col>
               <v-col cols="6" md="6">
-                <v-card-text
-                  >本条信息由{{ item.user_name }}({{
-                    item.email
-                  }})提供</v-card-text
-                >
+                <v-card-text>
+                  本条信息由{{ item.user_name }}({{
+                  item.email
+                  }})提供
+                </v-card-text>
               </v-col>
             </v-row>
           </v-card-actions>
@@ -85,9 +69,9 @@ export default {
       appInfoList: [],
       queryParam: {
         pagesize: 5,
-        pagenum: 1
+        pagenum: 1,
       },
-      total: 0
+      total: 0,
     }
   },
   created() {
@@ -99,13 +83,13 @@ export default {
       const { data: res } = await this.$http.get('appinfo/list', {
         params: {
           pagesize: this.queryParam.pagesize,
-          pagenum: this.queryParam.pagenum
-        }
+          pagenum: this.queryParam.pagenum,
+        },
       })
       this.appInfoList = res.data
       this.total = res.total
-    }
-  }
+    },
+  },
 }
 </script>
 <style></style>
